41ef8234314897828238ab18f46802b5975c39ed
yasainojikan <imtmtmhr2@gmail.com>
2019-03-02 17:44:01 +0900

    全ての機能を実装

--- taskapp/inputViewController.swift
+++ taskapp/inputViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import UserNotifications
 
 class inputViewController: UIViewController {
 
@@ -45,8 +46,44 @@ class inputViewController: UIViewController {
             self.task.date = self.datePicker.date
             self.realm.add(self.task, update: true)
         }
+        setNotification(task: task)
         
         super.viewWillDisappear(animated)
+    }
+    
+    func setNotification(task: Task) {
+        let content = UNMutableNotificationContent()
+        
+        if task.title == "" {
+            content.title = "(タイトルなし)"
+        } else {
+            content.title = task.title
+        }
+        if task.contents == "" {
+            content.body = "(内容なし)"
+        } else {
+            content.body = task.contents
+        }
+        content.sound = UNNotificationSound.default
+        
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching: dateComponents, repeats: false)
+        
+        let request = UNNotificationRequest.init(identifier: String(task.id),content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            print(error ?? "ローカル通知登録 OK")
+        }
+        
+        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/----------")
+                print(request)
+                print("----------/")
+            }
+        }
     }
 
     /*
