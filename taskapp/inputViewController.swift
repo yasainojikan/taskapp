4dec28ac44eb02189fb5d1cb108d9034b516cb17
yasainojikan <imtmtmhr2@gmail.com>
2019-03-02 15:36:02 +0900

    Realmをインストールする直前

---
+++ taskapp/inputViewController.swift
@@ -0,0 +1,34 @@
+//
+//  inputViewController.swift
+//  taskapp
+//
+//  Created by 今冨友裕 on 2019/03/02.
+//  Copyright © 2019年 yasainojikan. All rights reserved.
+//
+
+import UIKit
+
+class inputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
